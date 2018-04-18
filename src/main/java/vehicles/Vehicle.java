661bd0282ccc42540ac66a760e2665ff9725a8fb
natedaag <natedaage@yahoo.com>
2018-04-18 13:43:22 -0600

    a look at dependency injections using spring

---
+++ src/main/java/vehicles/Vehicle.java
@@ -0,0 +1,17 @@
+package vehicles;
+
+import parts.Engine;
+
+public class Vehicle {
+	
+	private Engine myEngine;
+	
+	public Vehicle(Engine anEngine) {
+		myEngine = anEngine;
+	}
+	
+	public void crankIgnition() {
+		myEngine.startEngine();
+	}
+
+}
